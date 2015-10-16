626365cedf41241292d3648f1fb7ec414a797f92
Paul S. Chun <paul@sixofhearts.us>
2015-10-16 20:40:51 +0200

    copy over elements from gulp server

--- ykimcpa/source/javascripts/all.js
+++ ykimcpa/source/javascripts/all.js
@@ -1,1 +1,11 @@
 //= require_tree .
+
+document.addEventListener("scroll", function(event) {
+  var _header = document.querySelector("header");
+  if (window.pageYOffset === 0) {
+    _header.className = "";
+  }
+  else {
+    _header.className = "below-zero";
+  }
+});
