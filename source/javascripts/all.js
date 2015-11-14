ae7a219179c6f3a76d3ffff266ffa07a9bc0ab67
Paul S. Chun <paul@sixofhearts.us>
2015-11-14 21:40:56 +0100

    initial gh-pages commit

---
+++ source/javascripts/all.js
@@ -0,0 +1,11 @@
+//= require bootstrap.min
+
+document.addEventListener("scroll", function(event) {
+  var _header = document.querySelector("header");
+  if (window.pageYOffset === 0) {
+    _header.className = "";
+  }
+  else {
+    _header.className = "below-zero";
+  }
+});
